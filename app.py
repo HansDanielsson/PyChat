cdec97ed9cd6bd274de7bf582501fe7a7b9e911c
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2025-03-29 10:38:24 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import pymongo
+
+# Connect to mongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017")
+
+db = client["PyChat"]
+message_col = db["messages"]
+
+# Tkinter GUI
+root = tk.Tk()
+root.title("PyChat")
+root.geometry("400x400")
+
+entry = tk.Entry(root, width=40)
+entry.pack(pady=5)
+
+
+root.mainloop()
